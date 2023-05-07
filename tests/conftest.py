a5395ab58770a691558e948db1f959f579fd8830
Sateeshkumar Varma <sateeshkumar.varma20@gmail.com>
2023-05-07 13:19:32 +0530

    Updated details as per SAAS

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import allure
 import logging
@@ -13,8 +14,10 @@ def setup_driver(request: webdriver):
     options.add_experimental_option('useAutomationExtension', False)
     # options.add_argument("--headless")
     options.add_argument('ignore-certificate-errors')
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=options)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     request.cls.driver = driver
     yield driver
     driver.close()
     driver.quit()
+
+    driver.find_element(By.ID,"user_security_key").send_keys()
--- tests/login_test.py
+++ tests/login_test.py
@@ -1,10 +1,11 @@
 import pytest
 from allure_commons.types import AttachmentType
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 import allure
 import logging
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 @pytest.mark.usefixtures("setup_driver")
 class TestDemo:
@@ -13,7 +14,17 @@ class TestDemo:
         log = logging.getLogger()
         log.setLevel(logging.INFO)
         log.info("Launch Application")
-        self.driver.get("https://subscription.packtpub.com/login")
+        self.driver.get("https://dev-sascental.azurewebsites.net/userlogin")
         allure.attach(self.driver.get_screenshot_as_png(),name="screenshot",attachment_type=AttachmentType.PNG)
         self.driver.maximize_window()
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "user_security_key"))
+        )
+        self.driver.find_element(By.ID, "user_security_key").send_keys("RGP3PG")
+        self.driver.find_element(By.ID, "user_emai_id").send_keys("man11111ish+831_s3@gmail.com")
+        self.driver.find_element(By.ID, "user_password").send_keys("12345")
+        self.driver.find_element(By.XPATH, "//span[text()='Sign In']/..").click()
+
+        time.sleep(5)
+
 
